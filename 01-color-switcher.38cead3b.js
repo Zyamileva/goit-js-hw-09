69e21dead5b3c55c897bf7f3cfbb0c41f6e1af47
Zyamileva <59625678+Zyamileva@users.noreply.github.com>
2023-10-27 21:23:08 +0000

    Deploying to gh-pages from @ Zyamileva/goit-js-hw-09@8d788976a1d1a212f9b12f5df7d614a9a105a5b2 🚀

---
+++ 01-color-switcher.38cead3b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}));var a=null}();
+//# sourceMappingURL=01-color-switcher.38cead3b.js.map
---
+++ 01-color-switcher.f673a318.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(){n=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));let n=null;
+//# sourceMappingURL=01-color-switcher.f673a318.js.map
